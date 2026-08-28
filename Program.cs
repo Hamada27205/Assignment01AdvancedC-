b41e83225c9c79d8d9ce7d5f943f99d50e8542f2
Hamada27205 <ahmedfarag8273@gmail.com>
2026-08-29 01:02:46 +0300

    Question08

--- Program.cs
+++ Program.cs
@@ -64,12 +64,20 @@ namespace Assignment01AdvancedC_
 
             #region Q7
 
-            //Accessable Bec this value type Only
+            //Accessible Bec this value type Only
             //Person<int>  P1 = new Person<int>(2);
             //Console.WriteLine(P1.Value);
 
             //Person<double>  P2 = new Person<double>(3.14);
             //Console.WriteLine(P2.Value);
+
+            #endregion
+
+            #region Q8
+
+            // Accessible reference type only
+            //Inventory<string> I1 = new Inventory<string>("Ahmed");
+            //Console.WriteLine(I1.Value);
 
             #endregion
 
